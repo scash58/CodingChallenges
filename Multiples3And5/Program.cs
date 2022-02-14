c19dd5d51e5fc71dd490bed4154ce4f2a0464dac
scash58 <scashion58@gmail.com>
2022-02-14 15:15:57 -0600

    Created a app to find the sum of all the multiples of 3 or 5 below 1000 and print it to the console.

---
+++ Multiples3And5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int sum = 0;
+
+            while (num < 1000)
+            {
+                //check if num is divisable by 3 and adds it to the sum
+                if(num % 3 == 0)
+                {
+                    //Console.WriteLine(num);
+                    sum += num;
+                }
+
+                //checks if num is divisable by 5 and adds it to the sum
+                if (num % 5 == 0)
+                {
+                    //Console.WriteLine(num);
+                    sum += num;
+                }
+
+                num++;
+            }
+
+            Console.WriteLine($"Sum of all the multiples of 3 or 5 below 1000: {sum:n0}");
+
+        }
+    }
+}
